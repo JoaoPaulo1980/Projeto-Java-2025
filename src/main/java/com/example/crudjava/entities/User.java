6749f1d5762e2a88f478fefa4f684a6853e2083e
Jpaulo <paulitu777@gmail.com>
2025-05-25 01:01:12 -0300

    utilizando o banco de dados H2

--- src/main/java/com/example/crudjava/entities/User.java
+++ src/main/java/com/example/crudjava/entities/User.java
@@ -1,20 +1,31 @@
 package com.example.crudjava.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "tb_user")
 
 public class User {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
+
     private  String name;
     private  String email;
     private  String phone;
-    private LocalDate birthDate;
     private  String password;
+    private LocalDate birthDate;
 
     public User(){
     }
 
     public User(long id, String name, String email, String phone, LocalDate birthDate, String password) {
+
         this.id = id;
         this.name = name;
         this.email = email;
